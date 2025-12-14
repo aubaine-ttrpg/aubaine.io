07793580651a9fa0682554e10973c0abc73a6dc9
Kori-San <kyoya.aymen@gmail.com>
2025-12-14 19:07:47 +0100

    🎉Restart for the 38th time !

--- webpack.config.js
+++ webpack.config.js
@@ -25,9 +25,6 @@ Encore
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
-
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
@@ -41,24 +38,39 @@ Encore
      */
     .cleanupOutputBeforeBuild()
 
-    // enable PostCSS support
-    .enablePostCssLoader()
+    // Displays build status system notifications to the user
+    // .enableBuildNotifications()
 
-    // Copy images
-    .copyFiles({
-    from: './assets/images',
-    to: 'images/[path][name].[hash:8].[ext]'
-    })
-
-    // enables hashed filenames (e.g. app.abc123.css)
     .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
+
+    // configure Babel
+    // .configureBabel((config) => {
+    //     config.plugins.push('@babel/a-babel-plugin');
+    // })
 
     // enables and configure @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
         config.corejs = '3.38';
     })
+
+    // enables Sass/SCSS support
+    //.enableSassLoader()
+
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
+
+    // uncomment if you use React
+    //.enableReactPreset()
+
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    //.enableIntegrityHashes(Encore.isProduction())
+
+    // uncomment if you're having problems with a jQuery plugin
+    //.autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
