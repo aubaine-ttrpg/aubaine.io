3602a722cd608bc9ae27ea2460deba8a4885bce6
a-y-men <aymen@layan.eu>
2025-12-16 23:18:35 +0100

    🔧 Update Tailwind configuration to include additional safelist colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
         './assets/**/*.{js,ts,jsx,tsx,css}',
         './templates/**/*.twig',
     ],
+    safelist: [
+        'bg-emerald-50', 'border-emerald-900', 'text-emerald-950',
+        'bg-red-50', 'border-red-900', 'text-red-950',
+        'bg-yellow-50', 'border-yellow-900', 'text-yellow-950',
+        'bg-indigo-50', 'border-indigo-900', 'text-indigo-950',
+        'bg-orange-50', 'border-orange-900', 'text-orange-950',
+        'bg-stone-50', 'border-stone-900', 'text-stone-950',
+        'border-emerald-900', 'border-red-900', 'border-yellow-900', 'border-indigo-900', 'border-orange-900', 'border-stone-900',
+        'text-emerald-900', 'text-red-900', 'text-yellow-900', 'text-indigo-900', 'text-orange-900', 'text-stone-900',
+    ],
     theme: {
         extend: {},
     },
