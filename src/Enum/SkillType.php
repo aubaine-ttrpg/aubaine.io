f1b1084fa887a12be4cca35e318f9af37d325e8b
a-y-men <aymen@layan.eu>
2025-12-16 23:27:04 +0100

    💄 Update SkillType icons and colors; enhance export result layout and translations

--- src/Enum/SkillType.php
+++ src/Enum/SkillType.php
@@ -14,12 +14,32 @@ enum SkillType: string
     public function icon(): string
     {
         return match ($this) {
-            self::ACTION => 'tabler:run',
-            self::BONUS => 'tabler:plus',
-            self::REACTION => 'tabler:bolt',
-            self::ATTACK => 'tabler:target-arrow',
-            self::PASSIVE => 'tabler:circle-dotted',
-            self::NONE => 'tabler:ban',
+            self::ACTION => 'material-symbols:circle',
+            self::BONUS => 'carbon:circle-filled',
+            self::REACTION => 'material-symbols:square-rounded',
+            self::ATTACK => 'mingcute:sword-fill',
+            self::PASSIVE => 'tabler:hexagon-filled',
+            self::NONE => 'tabler:hexagon-filled',
+        };
+    }
+
+    public function color(): string
+    {
+        return match ($this) {
+            self::ACTION => 'red',
+            self::BONUS => 'yellow',
+            self::REACTION => 'indigo',
+            self::ATTACK => 'orange',
+            self::PASSIVE => 'emerald',
+            self::NONE => 'stone',
+        };
+    }
+
+    public function iconClass(): string
+    {
+        return match ($this) {
+            self::ATTACK => '-rotate-45',
+            default => '',
         };
     }
 }
