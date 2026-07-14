e26408e793c3ee7af62842eacac09a00f161afeb
a-y-men <aymen@layan.eu>
2026-07-14 15:12:33 +0200

    ➕ (catalyst): add the sensiolabs gotenberg bundle
    Engine for server-side PDF rendering of the printable book. Pulls in
    symfony/mime and symfony/polyfill-intl-idn as transitive dependencies;
    composer audit reports no advisories.

--- catalyst/config/bundles.php
+++ catalyst/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
+    Sensiolabs\GotenbergBundle\SensiolabsGotenbergBundle::class => ['all' => true],
 ];
